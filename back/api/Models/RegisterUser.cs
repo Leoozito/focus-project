baae760a40fc2d361fc3136c50db819a7a1e0219
Leoozito <leonardonogueira616@gmail.com>
2024-03-13 16:22:22 -0300

    Estrutura de registro feito

---
+++ back/api/Models/RegisterUser.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class RegisterUser
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string telephone { get; set; }
+        public string email { get; set; }
+
+        public string cep { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        public string locationAdress { get; set; }
+        public int locationNumber { get; set; }
+        public string state { get; set; }
+        public string complementation { get; set; }
+
+        public DateTime registeredDate { get; set; } = DateTime.Now;
+    }
+}
